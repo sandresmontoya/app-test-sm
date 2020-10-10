913a9aff2bdfc7093245e9d1b2743a57022e6f0b
sandresmontoya <sandres.montoya@udea.edu.co>
2020-10-10 11:27:17 -0500

    jelo 2

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Test</title>
  </head>
  <body>
- <?php echo '<h1>Jelo</h1>'; ?>
+ <?php echo '<h1>ola vv djame jakearte el cora '.htmlspecialchars($_GET["name"]).'</h1>'; ?> 
  </body>
 </html>
